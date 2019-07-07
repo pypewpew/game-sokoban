d879a004c13489aba64cf340a014cccfdbedc30b
Radomir Dopieralski <openstack@sheep.art.pl>
2019-07-07 19:23:46 +0200

    Allow pushing crates off their desitinations

--- sokoban.py
+++ sokoban.py
@@ -4,7 +4,7 @@ import pew
 pew.init()
 screen = pew.Pix.from_iter((
     (1, 1, 1, 1, 1, 1, 1, 1),
-    (1, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 4, 0, 0, 1),
     (1, 1, 3, 1, 0, 0, 0, 1),
     (1, 0, 0, 1, 0, 1, 1, 1),
     (1, 0, 0, 3, 0, 2, 0, 1),
@@ -12,10 +12,16 @@ screen = pew.Pix.from_iter((
     (1, 0, 0, 0, 0, 1, 0, 1),
     (1, 1, 1, 1, 1, 1, 1, 1),
 ))
-x = 1
-y = 1
+
 blink = 1
-last = screen.pixel(x, y)
+last = 0
+for y in range(8):
+    for x in range(8):
+        if screen.pixel(x, y) == 4:
+            break;
+    else:
+        continue
+    break
 
 while True:
     screen.pixel(x, y, last)
@@ -33,15 +39,16 @@ while True:
     if screen.pixel(x + dx, y + dy) in {0, 2}:
         x += dx
         y += dy
-    elif screen.pixel(x + dx, y + dy) == 3:
-        if screen.pixel(x + dx + dx, y + dy + dy) == 1:
-            pass
-        else:
+    elif screen.pixel(x + dx, y + dy) in (3, 7):
+        if screen.pixel(x + dx + dx, y + dy + dy) in {0, 2}:
             if screen.pixel(x + dx + dx, y + dy + dy) == 2:
-                screen.pixel(x + dx + dx, y + dy + dy, 1)
+                screen.pixel(x + dx + dx, y + dy + dy, 7)
             else:
                 screen.pixel(x + dx + dx, y + dy + dy, 3)
-            screen.pixel(x + + dx, y + dy, 0)
+            if screen.pixel(x + dx, y + dy) == 3:
+                screen.pixel(x + + dx, y + dy, 0)
+            else:
+                screen.pixel(x + + dx, y + dy, 2)
             x += dx
             y += dy
     last = screen.pixel(x, y)
@@ -54,7 +61,7 @@ while True:
         break
     else:
         break
-    screen.pixel(x, y, 2 + blink)
+    screen.pixel(x, y, 3 if blink else 0)
     blink = 0 if blink else 1
     pew.show(screen)
     pew.tick(1 / 6)
